f64879c2b7c565beb9456d6afdc861e2aad540e3
Blake Loring <blake@parsed.uk>
2023-12-01 17:54:03 +0800

    type b tests

--- src/instruction_util.rs
+++ src/instruction_util.rs
@@ -19,10 +19,14 @@ const SIGN_BIT: u32 = 0b1000_0000_0000_0000_0000_0000_0000_0000;
 const LOWEST_7_BITS: u32 = 0b111_1111;
 const LOWEST_6_BITS: u32 = 0b11_1111;
 const LOWEST_5_BITS: u32 = 0b1_1111;
+const LOWEST_4_BITS: u32 = 0b1111;
 const LOWEST_3_BITS: u32 = 0b111;
 
 mod decoder {
-    use super::{extract, LOWEST_3_BITS, LOWEST_5_BITS, LOWEST_6_BITS, LOWEST_7_BITS, SIGN_BIT};
+    use super::{
+        extract, LOWEST_3_BITS, LOWEST_4_BITS, LOWEST_5_BITS, LOWEST_6_BITS, LOWEST_7_BITS,
+        SIGN_BIT,
+    };
 
     /// Extract the opcode (lowest 7 bits of the 32 bits)
     pub const fn opcode(instruction: u32) -> u8 {
@@ -102,6 +106,17 @@ mod decoder {
         let upper_6_bits = extract(instruction, 25, LOWEST_6_BITS) << 5;
         let lower_5_bits = extract(instruction, 7, LOWEST_5_BITS);
         sign_extend_32(upper_6_bits | lower_5_bits, 11, sign_bit)
+    }
+
+    /// B-type immediates are sign extended 13 bit immediates packed into bits (8-11) for imm(1:4),
+    /// bits 25-30 for imm(5:10), bit 7 for imm(11) and bit 31 for imm(12). imm(0) is always 0
+    /// (inclusive).
+    pub const fn b_type_immediate_32(instruction: u32) -> i32 {
+        let sign_bit = extract(instruction, 0, SIGN_BIT) != 0;
+        let bit_11 = extract(instruction, 7, 0b1) << 10;
+        let bits_10_to_5 = extract(instruction, 25, LOWEST_6_BITS) << 6;
+        let bits_4_to_1 = extract(instruction, 8, LOWEST_4_BITS) << 1;
+        sign_extend_32(bit_11 | bits_10_to_5 | bits_4_to_1, 12, sign_bit)
     }
 
     #[cfg(test)]
@@ -284,6 +299,52 @@ mod decoder {
             assert_eq!(s_type_immediate_32(MINUS_TWO_HUNDRED_AND_FIFTY), -250);
             assert_eq!(s_type_immediate_32(MINUS_ONE), -1);
             assert_eq!(s_type_immediate_32(MIN_NEGATIVE_VALUE), -2048);
+        }
+
+        #[test]
+        fn test_b_type_immediate() {
+            const ZERO: u32 = 0b0000_0001_0101_1111_0001_0000_0101_0110;
+
+            const fn pack(v: u32) -> u32 {
+                if v & 1 != 0 {
+                    panic!("impossible to pick, lowest bit is set");
+                }
+
+                // The input is 13 bits compressed into 12 (assuming v & 1 == 0)
+                let v = (v & 0b1_1111_1111_1111) >> 1;
+
+                let sign_bit = (v & 0b1000_0000_0000) >> 11;
+                let bit_eleven = (v & 0b0100_0000_0000) >> 10;
+                let bits_ten_to_five = (v & 0b0001_1111_0000) >> 4;
+                let lower_four_bits = v & 0b1111;
+
+                ZERO | (sign_bit << 31)
+                    | (bit_eleven << 6)
+                    | (lower_four_bits << 7)
+                    | (bits_ten_to_five << 25)
+            }
+
+            // Test positive values
+            const TWO: u32 = pack(2);
+            const TWO_HUNDRED_AND_FIFTY: u32 = pack(250);
+            const MAX_POSITIVE_VALUE: u32 = pack(4096);
+
+            assert_eq!(b_type_immediate_32(ZERO), 0);
+            assert_eq!(b_type_immediate_32(TWO), 2);
+            assert_eq!(b_type_immediate_32(TWO_HUNDRED_AND_FIFTY), 250);
+            assert_eq!(b_type_immediate_32(MAX_POSITIVE_VALUE), 4096);
+
+            const fn pack_signed(v: i32) -> u32 {
+                pack(v as u32)
+            }
+
+            const MINUS_TWO: u32 = pack_signed(-2);
+            const MINUS_TWO_HUNDRED_AND_FIFTY: u32 = pack_signed(-250);
+            const MIN_NEGATIVE_VALUE: u32 = pack_signed(-4096);
+
+            assert_eq!(b_type_immediate_32(MINUS_TWO_HUNDRED_AND_FIFTY), -250);
+            assert_eq!(b_type_immediate_32(MINUS_TWO), -2);
+            assert_eq!(b_type_immediate_32(MIN_NEGATIVE_VALUE), -4096);
         }
     }
 }
